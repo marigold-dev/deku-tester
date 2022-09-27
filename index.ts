5056ae56285dc3b5b228deff5ecedfb16b0da686
Felipe Cardozo <fraifelipe@gmail.com>
2022-09-26 22:16:33 -0300

    add dotenv support

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import { TezosToolkit } from "@taquito/taquito";
-import { InMemorySigner } from "@taquito/signer";
+// import { TezosToolkit } from "@taquito/taquito";
+// import { InMemorySigner } from "@taquito/signer";
 // import axios from "axios";
 import chalk from "chalk";
 // import dayjs from "dayjs";
+import * as dotenv from 'dotenv';
+dotenv.config()
 
-const mainnet = "https://mainnet.tezos.marigold.dev/";
-const jakartanet = "https://jakartanet.tezos.marigold.dev/";
+// const mainnet = "https://mainnet.tezos.marigold.dev/";
+// const jakartanet = "https://jakartanet.tezos.marigold.dev/";
 // const kathmandunet = "https://kathmandunet.tezos.marigold.dev/";
 // const kathmandunet = "https://kathmandunet-1.tezos.marigold.dev/";
-const ghostnet = "https://ghostnet.tezos.marigold.dev/";
+// const ghostnet = "https://ghostnet.tezos.marigold.dev/";
 
 // const Tezos = new TezosToolkit(ghostnet);
 // Tezos.setProvider({
@@ -19,6 +21,7 @@ const ghostnet = "https://ghostnet.tezos.marigold.dev/";
 
 async function main(): Promise<void> {
 	console.log(chalk.green("Hello World!"));
+	console.log(process.env.WALLET_PRIVATE_KEY);
 }
 
 main().catch(console.error);
